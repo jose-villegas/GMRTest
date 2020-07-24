a27c6009465959172d85040a74fbeae396541b09
José Villegas <villegasjose.gg@gmail.com>
2020-07-24 15:14:13 -0400

    Added JSON parser., TMP, starting table structure class

---
+++ Assets/Source/TableStructure.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[Serializable]
+public class TableStructure
+{
+    private string _title;
+    private List<string> _headers;
+    private List<Dictionary<string, string>> _data;
+
+    public string Title
+    {
+        get => _title;
+        set => _title = value;
+    }
+
+    public List<string> ColumnHeaders
+    {
+        get => _headers;
+        set => _headers = value;
+    }
+
+    public List<Dictionary<string, string>> Data
+    {
+        get => _data;
+        set => _data = value;
+    }
+}
