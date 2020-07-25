6ffb7a76c4238e64880a01cc06e36af3bf3dac7d
José Villegas <villegasjose.gg@gmail.com>
2020-07-25 14:49:41 -0400

    Loading with Json.NET, working on small components

---
+++ Assets/Source/TableStructureLoader.cs
@@ -0,0 +1,25 @@
+﻿using System.IO;
+using Newtonsoft.Json;
+using UnityEngine;
+
+public class TableStructureLoader : MonoBehaviour
+{
+    private const string FileName = "JsonChallenge.json";
+    private TableStructure _tableStructure;
+
+    private void Awake()
+    {
+        LoadTable();
+    }
+
+    private void LoadTable()
+    {
+        var path = Application.streamingAssetsPath;
+        path = Path.Combine(path, FileName);
+
+        var content = System.IO.File.ReadAllText(path);
+
+        // try to deserialize
+        _tableStructure = JsonConvert.DeserializeObject<TableStructure>(content);
+    }
+}
